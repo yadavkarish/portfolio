b3f00ebb07fdea3a5c2534b0394d73a5e2a18236
Karishma <karishma@beanbyte.in>
2023-09-09 12:29:17 +0530

    complete portfolio

---
+++ main.js
@@ -0,0 +1,24 @@
+// // Function to reset the form
+// function resetForm() {
+//     document.getElementById("myForm").reset();
+// }
+
+// // Submit the form and reset it afterward
+// document.getElementById("myForm").addEventListener("submit", function (event) {
+//     event.preventDefault(); // Prevent the default form submission
+
+//     // Your code to send the form data here (e.g., AJAX request)
+
+//     // Reset the form
+//     resetForm();
+// });
+
+var typed = new Typed(".text",{
+    strings:["Web Developer","PHP Developer","Java Developer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true 
+});
+
